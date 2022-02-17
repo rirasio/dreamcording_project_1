9e39acf69dc9b8c68d03aa6a33fde36ee1b32353
Jinhyeong Choi <79887979+jinchoi9167@users.noreply.github.com>
2022-02-17 19:06:30 +0900

    ready parameter

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 console.log('-----------app start------------');
+console.log();
 
 const fs = require('fs').promises;
 
-console.log('argv');
-console.log(process.argv);
+console.log('-----------root path');
+console.log(__dirname);
 
-console.log('file list');
+const folderName = process.argv[2] === undefined ? '' : process.argv[2];
+
+console.log('-----------target folder');
+console.log(folderName);
+
+console.log('-----------file list');
 fs.readdir('./test')
 .then((array) => { console.log(array) })
 .catch(console.error);
