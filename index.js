2eee8392c0d16e27bb93b640628b92d327863234
Jinhyeong Choi <79887979+jinchoi9167@users.noreply.github.com>
2022-02-22 18:16:43 +0900

    테스트, 파일 이동 후 삭제

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
-console.log('-----------app start------------');
-console.log();
-
 const fs = require('fs').promises;
 
-console.log('-----------root path');
-console.log(__dirname);
+const targetFolderPath = `${__dirname}/${process.argv[2] === undefined ? 'test' : process.argv[2]}`;
 
-const folderName = process.argv[2] === undefined ? '' : process.argv[2];
+fs.stat(`${targetFolderPath}/video`)
+.catch(() => {
+  fs.mkdir(`${targetFolderPath}/video`).catch(console.error);
+});
 
-console.log('-----------target folder');
-console.log(folderName);
+fs.stat(`${targetFolderPath}/captured`)
+.catch(() => {
+  fs.mkdir(`${targetFolderPath}/captured`).catch(console.error);
+});
 
-console.log('-----------file list');
-fs.stat(`${__dirname}/${folderName}/video`)
-.then((states) => {
-  console.log(states);
-})
-.catch(console.error);
+fs.stat(`${targetFolderPath}/duplicated`)
+.catch(() => {
+  fs.mkdir(`${targetFolderPath}/duplicated`).catch(console.error);
+});
 
-fs.readdir('./test')
+const videoExtension = new Set(['mp4', 'mov']);
+const captureExtension = new Set(['png', 'aae']);
+const photoExtension = new Set(['jpg']);
+
+fs.readdir(targetFolderPath)
 .then((array) => {
   for (fileName of array) {
-    console.log(fileName);
+    let nameAndExtension = fileName.split('.');
+
+    if (videoExtension.has(nameAndExtension[1])) {
+      fs.link(`${targetFolderPath}/${fileName}`, `${targetFolderPath}/video/${fileName}`)
+      .then(() => {
+        fs.rm(`${targetFolderPath}/${fileName}`)
+        .catch(console.error);
+      })
+      .catch(console.error);
+
+      continue;
+    }
+
+    if (captureExtension.has(nameAndExtension[1])) {
+      fs.link(`${targetFolderPath}/${fileName}`, `${targetFolderPath}/captured/${fileName}`)
+      .then(() => {
+        fs.rm(`${targetFolderPath}/${fileName}`)
+        .catch(console.error);
+      })
+      .catch(console.error);
+
+      continue;
+    }
+
+    if (photoExtension.has(nameAndExtension[1])) {
+      if (nameAndExtension[0].split('_').pop().includes('E')) {
+        let targetFileName = `${nameAndExtension[0].split('_')[0]}_${nameAndExtension[0].split('_')[1].substr(1)}.${nameAndExtension[1]}`;
+        
+        fs.link(`${targetFolderPath}/${targetFileName}`, `${targetFolderPath}/duplicated/${targetFileName}`)
+        .then(() => {
+          fs.rm(`${targetFolderPath}/${targetFileName}`)
+          .catch(console.error);
+        })
+        .catch(console.error);
+      }
+
+      continue;
+    }
+    
   }
 })
 .catch(console.error);
