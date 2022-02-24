90f8c13d3d2b73916b315e6d1bcb4c01ed7b2065
Jinhyeong Choi <79887979+jinchoi9167@users.noreply.github.com>
2022-02-24 19:04:06 +0900

    final

--- index.js
+++ index.js
@@ -1,68 +1,53 @@
-const fs = require('fs').promises;
-
+const fs = require('fs');
+const path = require('path');
 const targetFolderPath = `${__dirname}/${process.argv[2] === undefined ? 'test' : process.argv[2]}`;
-
-fs.stat(`${targetFolderPath}/video`)
-.catch(() => {
-  fs.mkdir(`${targetFolderPath}/video`).catch(console.error);
-});
-
-fs.stat(`${targetFolderPath}/captured`)
-.catch(() => {
-  fs.mkdir(`${targetFolderPath}/captured`).catch(console.error);
-});
-
-fs.stat(`${targetFolderPath}/duplicated`)
-.catch(() => {
-  fs.mkdir(`${targetFolderPath}/duplicated`).catch(console.error);
-});
-
 const videoExtension = new Set(['mp4', 'mov']);
 const captureExtension = new Set(['png', 'aae']);
 const photoExtension = new Set(['jpg']);
 
-fs.readdir(targetFolderPath)
-.then((array) => {
-  for (fileName of array) {
-    let nameAndExtension = fileName.split('.');
+fs.stat(`${targetFolderPath}/video`, makeFolder);
+fs.stat(`${targetFolderPath}/captured`, makeFolder);
+fs.stat(`${targetFolderPath}/duplicated`, makeFolder);
+fs.readdir(targetFolderPath, {withFileTypes: true}, moveFile);
 
-    if (videoExtension.has(nameAndExtension[1])) {
-      fs.link(`${targetFolderPath}/${fileName}`, `${targetFolderPath}/video/${fileName}`)
-      .then(() => {
-        fs.rm(`${targetFolderPath}/${fileName}`)
-        .catch(console.error);
-      })
-      .catch(console.error);
+function makeFolder(err, stats) {
+  if (err) {
+    fs.promises.mkdir(`${targetFolderPath}/${err.path.split(path.sep).pop()}`).catch(console.error);
+  }
+  return;
+}
 
-      continue;
-    }
+function moveFile(err, files) {
+  if (err) {
+    console.error;
+  }
+  
+  for (dirent of files) {
+    if (dirent.isDirectory() === false) {
+      const ext = dirent.name.split('.').pop();
 
-    if (captureExtension.has(nameAndExtension[1])) {
-      fs.link(`${targetFolderPath}/${fileName}`, `${targetFolderPath}/captured/${fileName}`)
-      .then(() => {
-        fs.rm(`${targetFolderPath}/${fileName}`)
-        .catch(console.error);
-      })
-      .catch(console.error);
-
-      continue;
-    }
-
-    if (photoExtension.has(nameAndExtension[1])) {
-      if (nameAndExtension[0].split('_').pop().includes('E')) {
-        let targetFileName = `${nameAndExtension[0].split('_')[0]}_${nameAndExtension[0].split('_')[1].substr(1)}.${nameAndExtension[1]}`;
-        
-        fs.link(`${targetFolderPath}/${targetFileName}`, `${targetFolderPath}/duplicated/${targetFileName}`)
-        .then(() => {
-          fs.rm(`${targetFolderPath}/${targetFileName}`)
-          .catch(console.error);
-        })
-        .catch(console.error);
+      if (videoExtension.has(ext)) {
+        console.log('비디오 파일은 video 폴더로 이동합니다.');
+        fs.promises.rename(`${targetFolderPath}/${dirent.name}`, `${targetFolderPath}/video/${dirent.name}`).catch(console.error);
+        continue;
       }
 
-      continue;
+      if (captureExtension.has(ext)) {
+        console.log('스크린샷 파일은 captured 폴더로 이동합니다.');
+        fs.promises.rename(`${targetFolderPath}/${dirent.name}`, `${targetFolderPath}/captured/${dirent.name}`).catch(console.error);
+        continue;
+      }
+
+      if (photoExtension.has(ext)) {
+        const nameSet = new Set(dirent.name);
+        if (nameSet.has('E')) {
+          console.log('수정 사진의 원본은 duplicated 폴더로 이동합니다.');
+          nameSet.delete('E');
+          fs.promises.rename(`${targetFolderPath}/${[...nameSet].join('')}`, `${targetFolderPath}/duplicated/${[...nameSet].join('')}`).catch(console.error);
+          continue;
+        }
+      }
     }
-    
   }
-})
-.catch(console.error);
+  return;
+}
